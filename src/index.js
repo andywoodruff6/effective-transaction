f982cf92b7ca6a0c99194384f6b9166afcba6874
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2022-11-15 09:48:08 -0600

    added dotenv checked health

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+require("dotenv").config();
+
+const Blockfrost = require("@blockfrost/blockfrost-js");
+////////////////////////////////
+console.log("Hello, You're code has started");
+
+const API = new Blockfrost.BlockFrostAPI({
+  projectId: process.env.BLOCKFROST_API_KEY,
+});
+
+////////////////////////////////
+const main = async () => {
+  const health = await API.health();
+  console.log(health);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
